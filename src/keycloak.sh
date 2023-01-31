aceeddb319973c64082d39b204edcbe5eea653c0
JohnSt99 <jsm@hotmail.gr>
2023-01-31 14:34:29 +0200

    Fix hardcoded keycloak url

--- src/keycloak.sh
+++ src/keycloak.sh
@@ -1,5 +1,5 @@
 echo "Waiting for keycloak to fully start..."
-while [ "$(curl --location --silent --output /dev/null -w "%{http_code}" --show-error keycloak:8080/auth)" != "200" ]; do sleep 1; done
+while [ "$(curl --location --silent --output /dev/null -w "%{http_code}" --show-error ${KEYCLOAK_ADDRESS}:8080/auth)" != "200" ]; do sleep 1; done
 echo "Keycloak has started!"
 echo "Obtaining token..."
 token=$(curl --silent --data "username=${KEYCLOAK_ADMIN}&password=${KEYCLOAK_ADMIN_PASSWORD}&grant_type=password&client_id=admin-cli" ${KEYCLOAK_ADDRESS}/realms/master/protocol/openid-connect/token)
