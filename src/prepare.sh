9ede78aa0e55e85456146733b2b67e59abc5a161
JohnSt99 <jsm@hotmail.gr>
2023-09-27 15:53:49 +0300

    Update prepare.sh
    Fixed environment mode for evolved5g registering

--- src/prepare.sh
+++ src/prepare.sh
@@ -2,6 +2,6 @@ jq -r .capif_host=\"$CAPIF_HOSTNAME\" capif_registration.json >> tmp.json && mv 
 jq -r .capif_http_port=\"$CAPIF_PORT_HTTP\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 jq -r .capif_https_port=\"$CAPIF_PORT_HTTPS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 jq -r .capif_callback_url=\"http://$CALLBACK_ADDRESS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
-evolved5g register-and-onboard-to-capif --config_file_full_path="/app/capif_registration.json"
-sh ./keycloak.sh --environment="$ENVIRONMENT_MODE"
+evolved5g register-and-onboard-to-capif --config_file_full_path="/app/capif_registration.json" --environment="$ENVIRONMENT_MODE"
+sh ./keycloak.sh 
 python3 -u NetApp-v3.py
